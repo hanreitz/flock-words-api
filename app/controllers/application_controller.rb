0c5a49075fa2eb41173bf481b5df302ec6f1dce3
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-08-26 14:49:59 -0400

    adds authorization header parsing and current user assignment to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,30 @@ class ApplicationController < ActionController::API
     JWT.encode(payload, 'my_s3cr3t') # obviously change this to a real gitignore secret
   end
 
-  def decoded_token(token)
-    JWT.decode(token, 'my_s3cr3t')[0] # again obviously this will change
+  def auth_header
+    request.headers['Authorization']
   end
-  
+
+  def decoded_token(token)
+    if auth_header
+      token = auth_header.split(' ')[1]
+      begin
+        JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256') # again obviously this will change
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
+  def current_user
+    if decoded_token
+      user_id = decoded_token[0]['user_id']
+      @user = User.find_by(id: user_id)
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
 end
