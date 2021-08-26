cb18ebe5b616f17fac6ccb49a1a8e80efdcc6ca1
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-08-26 18:59:32 -0400

    edits migration of password_digest to be correct for bcrypt use

--- db/migrate/20210826164424_create_users.rb
+++ db/migrate/20210826164424_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :email
-      t.password_digest :password
+      t.string :password_digest
 
       t.timestamps
     end
