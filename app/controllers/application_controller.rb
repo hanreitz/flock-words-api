dd31d30ce6d526bc7a0b9fba43fe69f4e7142c5b
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-08-26 12:52:23 -0400

    adds JWT encoding and decoding to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  def encode_token(payload)
+    JWT.encode(payload, 'my_s3cr3t') # obviously change this to a real gitignore secret
+  end
+
+  def decoded_token(token)
+    JWT.decode(token, 'my_s3cr3t')[0] # again obviously this will change
+  end
+  
 end
