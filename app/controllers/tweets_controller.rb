e43ced57c0d55df8598367304c4f4860f84ee1fe
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-09-05 14:40:44 -0400

    creates custom route /data connected to TweetsController#get_data for wordcloud and data table on frontend

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -24,6 +24,24 @@ class TweetsController < ApplicationController
     end
   end
 
+  def get_data
+    sentence_list = Tweet.all.map{ |tweet| tweet.content }
+    exclusion_words = "is the be to of and a in that have I I'm I've I'd it for not on with he as you do at this but his by from they we say her she or an will my one all would there their what so up out if about who get which go me when make can like time no just him know take people into year your good some could them see other than then now look only come its over think also back after use two how our work first well way even new want because any these give day most us hasn't ain't aren't could've would've should've couldn't didn't had've he'll how'll how're how's might've she'd he'd she'll they're they're we're we've can't won't shouldn't wouldn't haven't I'm ? .".downcase().split(' ')
+    word_list = sentence_list.map{ |sentence| sentence.split(' ')}.flatten.map{ |word| word.downcase() }
+    filtered_words = word_list.filter{ |word| !exclusionWords.include?(word) }.filter{ |word| !word.include?('@') }.filter{ |word| !word.includes('http')}
+    word_object = []
+    filtered_words.each do |filtered_word|
+      existing_word = word_object.find{ |word| word.text == filtered_word }
+      index = word_object.find_index{ |word| word.text == filtered_word }
+      if existing_word
+        word_object = word_object.slice(0,index).concat({ text: word_object[index].text, value: (word_object[index].value + 1)}).concat(word_object.slice(index+1))
+      } else {
+        word_object = word_object.concat({ text: filtered_word, value: 1 })
+      }
+    }
+    render json: word_object
+  end
+
   # PATCH/PUT /tweets/1
   def update
     if @tweet.update(tweet_params)
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   end
   resources :users
 
+  get '/data' to: "tweets#get_data"
   root 'feeds#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
